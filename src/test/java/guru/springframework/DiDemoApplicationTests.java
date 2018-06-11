d45a3c82d9bf6bcd004aa609ef00563ea0c93b8d
David Albalate <david@netshell.co.uk>
2018-06-11 15:30:40 +0100

    First step on assigment

--- src/test/java/guru/springframework/DiDemoApplicationTests.java
+++ src/test/java/guru/springframework/DiDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package udemy.spreingboot.webdemo;
+package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebdemoApplicationTests {
+public class DiDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
